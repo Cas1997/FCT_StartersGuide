71f849cf200191b71f12c27a380f22d154cf0b80
Cas van Veen - They/them <96796377+Cas1997@users.noreply.github.com>
2025-07-10 11:27:18 +0200

    Fix prepare_env.sh

--- Run_Simulation/prepare_env.sh
+++ Run_Simulation/prepare_env.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-export ALIBUILD_WORK_DIR=/misc/alidata150/alice_u/cas/O2Simulation/alice/sw
+export ALIBUILD_WORK_DIR=<path to alibuild work dir>
 eval `/usr/local/bin/alienv shell-helper`
 export PATH=${PATH}
 
 # o2_env.sh
 echo "#!/bin/bash" > o2_env.sh
 echo "" >> o2_env.sh
-alienv printenv O2/latest-retractable_IRIS-o2 >> o2_env.sh
+alienv printenv <your O2 environment> >> o2_env.sh
 sed -i "s|test 0;||g" o2_env.sh
 chmod a+x o2_env.sh
